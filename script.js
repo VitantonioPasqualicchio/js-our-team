2e8976d76d92789cc63882bbf6988a7c2b245eef
vitantonio99 <vitantoniopasqualicchio0@gmail.com>
2026-06-12 20:12:52 +0200

    project setup

---
+++ script.js
@@ -0,0 +1,38 @@
+const teamMembers = [
+  {
+    name: "Bill Gates",
+    role: "Designer",
+    email: "billgates@team.com",
+    img: "img/BillGates.jpg"
+  },
+  {
+    name: "Elon Musk",
+    role: "Front-end Developer",
+    email: "elonmusk@team.com",
+    img: "img/ElonMusk.jpg"
+  },
+  {
+    name: "Jeff Bezos",
+    role: "Back-end Developer",
+    email: "jeffbezos@team.com",
+    img: "img/JeffBezos.jpg"
+  },
+  {
+    name: "Mark Zuckerberg",
+    role: "SEO Specialist",
+    email: "markzuckerberg@team.com",
+    img: "img/MarkZuckerberg.jpg"
+  },
+  {
+    name: "Vitantonio Pasqualicchio",
+    role: "SEO Specialist",
+    email: "vitantoniopasqualicchio@team.com",
+    img: "img/VitantonioPasqualicchio.jpg"
+  },
+  {
+    name: "Lucia D'Abramo",
+    role: "Veterinary Doctor",
+    email: "luciadabramo@team.com",
+    img: "img/LuciaD'Abramo.jpeg"
+  }
+];
